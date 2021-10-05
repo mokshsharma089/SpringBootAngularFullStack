c83387a48492bf9b066c74f835727a16aed80fb6
mokshsharma089 <42546670+mokshsharma089@users.noreply.github.com>
2021-10-05 21:13:54 +0530

    Add files via upload

--- Backend/src/main/java/com/Backend/Backend/controller.java
+++ Backend/src/main/java/com/Backend/Backend/controller.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/employee")
 public class controller {
@@ -26,7 +28,7 @@ public class controller {
 	public List<EmployeeEntity> getAllEmployees(){
 		return repo.findAll();
 	}
-	
+	@CrossOrigin
 	@GetMapping(value="/id/{id}",produces = "application/json")
 	public EmployeeEntity getEmployee(@PathVariable("id") String tempId) {
 		Integer id=Integer.parseInt(tempId);
